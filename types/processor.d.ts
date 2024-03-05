0d02be9e6c340df1b2430b6d22234a691dc078c2
JP Yliuntinen <83706030+jpy-luke@users.noreply.github.com>
2024-03-05 19:49:25 +0200

    chore: add copy of Influence SDK product and process definitions
    From https://github.com/influenceth/sdk @ 343a82f
    To be replaced with an NPM import when possible to fiddle with missing TypeScript declarations

---
+++ types/processor.d.ts
@@ -0,0 +1,16 @@
+declare namespace _default {
+    export { IDS };
+    export { STATUSES };
+}
+export default _default;
+declare namespace IDS {
+    let REFINERY: number;
+    let FACTORY: number;
+    let BIOREACTOR: number;
+    let SHIPYARD: number;
+    let DRY_DOCK: number;
+}
+declare namespace STATUSES {
+    let IDLE: number;
+    let RUNNING: number;
+}
