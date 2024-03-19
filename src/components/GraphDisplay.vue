e83928e00c2ba47df4accd1c692b3b7d92944f63
JP Yliuntinen <83706030+jpy-luke@users.noreply.github.com>
2024-03-19 14:56:40 +0200

    ref: change to the Vue script setup style

--- src/components/GraphDisplay.vue
+++ src/components/GraphDisplay.vue
@@ -1,32 +1,24 @@
 <template>
   <div id="sigma-container"></div>
 </template>
-<script lang="ts">
+
+<script setup lang="ts">
 import Sigma from 'sigma'
 import { ProductionGraph } from '@/graphing/graph'
+import { onMounted } from 'vue'
 
-export default {
-  data() {
-    const graphHandler = new ProductionGraph()
-    graphHandler.initializeTotalGraph()
+const graphHandler = new ProductionGraph()
+graphHandler.initializeTotalGraph()
 
-    return {
-      graph: graphHandler.graph
-    }
-  },
-  setup() {
-  },
-  mounted() {
-    const container = document.getElementById('sigma-container') as HTMLElement
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    const renderer = new Sigma(
-      this.graph,
-      container,
-      { renderEdgeLabels: true, labelColor: { color: 'gray' } }
-    )
-  }
-}
-
+onMounted(() => {
+  const container = document.getElementById('sigma-container') as HTMLElement
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const renderer = new Sigma(
+    graphHandler.graph,
+    container,
+    { renderEdgeLabels: true, labelColor: { color: 'gray' } }
+  )
+})
 </script>
 
 <style scoped>
