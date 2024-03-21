7ee85e3366bf73245f4d42c7c993b0d8353067d8
JP Yliuntinen <83706030+jpy-luke@users.noreply.github.com>
2024-03-21 13:13:10 +0200

    feat: add possiblity to drag nodes
    code from vue tutorial at https://codesandbox.io/p/sandbox/github/jacomyal/sigma.js/tree/main/examples/mouse-manipulations?file=%2Findex.ts%3A42%2C1-71%2C4

--- src/components/GraphDisplay.vue
+++ src/components/GraphDisplay.vue
@@ -4,20 +4,44 @@
 
 <script setup lang="ts">
 import Sigma from 'sigma'
-import { ProductionGraph } from '@/graphing/graph'
 import { onMounted } from 'vue'
 import type Graph from 'graphology'
 
 const props = defineProps<{graph: Graph}>()
+let draggedNode: string | null = null;
+let isDragging = false;
 
 onMounted(() => {
   const container = document.getElementById('sigma-container') as HTMLElement
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const renderer = new Sigma(
     props.graph,
     container,
     { renderEdgeLabels: true, labelColor: { color: 'gray' } }
   )
+
+  renderer.on("downNode", (e) => {
+    isDragging = true;
+    draggedNode = e.node;
+    props.graph.setNodeAttribute(draggedNode, "highlighted", true);
+  });
+
+  renderer.getMouseCaptor().on("mousemovebody", (e) => {
+    if (!isDragging || !draggedNode) return;
+    const pos = renderer.viewportToGraph(e);
+    props.graph.setNodeAttribute(draggedNode, "x", pos.x);
+    props.graph.setNodeAttribute(draggedNode, "y", pos.y);
+    e.preventSigmaDefault();
+    e.original.preventDefault();
+    e.original.stopPropagation();
+  });
+
+  renderer.getMouseCaptor().on("mouseup", () => {
+    if (draggedNode) {
+      props.graph.removeNodeAttribute(draggedNode, "highlighted");
+    }
+    isDragging = false;
+    draggedNode = null;
+  });
 })
 </script>
 
